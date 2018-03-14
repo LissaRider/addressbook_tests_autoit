6e5189f6d72afb182eb113813387bdb4349b2457
vsmirnova <vsmirnova@VSMIRNOVA>
2018-03-14 14:05:11 +0300

    Check2

--- addressbook_tests_autoit/tests/GroupRemovalTests.cs
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -16,13 +16,11 @@ namespace addressbook_tests_autoit
             app.Groups.VerifyGroupPresence();
 
             List<GroupData> oldGroups = app.Groups.GetGroupList();
-
             app.Groups.Remove(0);
 
             Assert.AreEqual(oldGroups.Count - 1, app.Groups.GetGroupCount());
 
             List<GroupData> newGroups = app.Groups.GetGroupList();
-
             oldGroups.RemoveAt(0);
 
             Assert.AreEqual(oldGroups, newGroups);            
